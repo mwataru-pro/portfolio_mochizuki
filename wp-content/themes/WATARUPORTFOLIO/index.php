2b53c156e5d6e1482863224aef6f7c98cc9a5c69
mwataru-pro <m1130wata@gmail.com>
2022-02-16 11:22:04 +0900

    add ファーストビューの作成

--- wp-content/themes/WATARUPORTFOLIO/index.php
+++ wp-content/themes/WATARUPORTFOLIO/index.php
@@ -1,2 +1,19 @@
-<?php get_header(); ?>
+<?php
+/**
+ * This is index page
+ *
+ * @package portfolio_mochizuki
+ */
+
+get_header();
+?>
+<div id="first-view" class="p-first-view">
+	<div class="p-first-view__inner">
+		<h2 class="p-first-view__title">WATARU PORTFOLIO</h2>
+		<p class="p-first-view__image">
+			<img src="<?php echo esc_url(get_theme_file_uri('/assets/img/first-view1.png')); ?>" alt="man illust">
+		</p>
+		<div class="c-fluid"></div>
+	</div>
+</div>
 <?php get_footer(); ?>
